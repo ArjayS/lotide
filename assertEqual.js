280f57e1a43df9503ae85571f9fa602a8fba3941
Robert Servado <servadorobertgmail.com>
2022-05-21 19:56:42 -0400

    Add assertEqual funtction

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(
+      "The type of operand for actual is a " +
+        typeof actual +
+        ", and the type of operand for expected is " +
+        typeof expected
+    );
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+    console.log(
+      "The type of operand for actual is a " +
+        typeof actual +
+        ", and the type of operand for expected is " +
+        typeof expected
+    );
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
