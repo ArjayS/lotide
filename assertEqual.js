10916bc7dd953d6c78d7d5ade162b2cee993bbc5
Robert Servado <servadorobertgmail.com>
2022-05-21 20:11:06 -0400

    Changed the console.log output string sentences to adapt template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,19 +1,13 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     console.log(
-      "The type of operand for actual is a " +
-        typeof actual +
-        ", and the type of operand for expected is " +
-        typeof expected
+      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
     );
   } else {
-    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
     console.log(
-      "The type of operand for actual is a " +
-        typeof actual +
-        ", and the type of operand for expected is " +
-        typeof expected
+      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
     );
   }
 };
