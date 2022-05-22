88fcc6cc89bfce7e2aae379179e1831f8ca30a5a
Robert Servado <servadorobertgmail.com>
2022-05-22 10:34:10 -0400

    Add tail.js

---
+++ tail.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
+    );
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
+    );
+  }
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
