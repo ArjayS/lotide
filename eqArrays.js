fe7de911e20b22fdbf76a4fb2578c8852e625264
Robert Servado <servadorobertgmail.com>
2022-05-23 14:22:01 -0400

    Added notes on the eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -13,15 +13,19 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = function (array1, array2) {
+  // determining if both arrays are not equal in length
   if (array1.length !== array2.length) {
     return false;
   }
 
+  // determining if each of the elements (either number or string) are not the same
   for (let i = 0; i <= array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
+
+  // if conditions above are not false the function will return true
   return true;
 };
 
