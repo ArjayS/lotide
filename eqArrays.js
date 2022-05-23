f345c1863ee4735c79d27ebfe0f9864508a92426
Robert Servado <servadorobertgmail.com>
2022-05-23 13:55:59 -0400

    Add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
+    );
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
+    );
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+////////////////////////////////////////////////////
+console.log("-----------------------------------");
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+console.log("-----------------------------------");
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+console.log("-----------------------------------");
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+console.log("-----------------------------------");
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+console.log("-----------------------------------");
