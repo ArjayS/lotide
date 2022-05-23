907120423b9a28ae25c858c7d8428b1fff56bbcc
Robert Servado <servadorobertgmail.com>
2022-05-23 19:01:26 -0400

    Editted the previous submission because I realized my assumption on the question was wrong

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,17 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    console.log(
-      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
-    );
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion: Passed`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-    console.log(
-      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
-    );
+    console.log(`❌❌❌ Assertion: Failed`);
   }
 };
 
 const eqArrays = function (array1, array2) {
+  // determining if both arrays are not equal in length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
   // determining if each of the elements (either number or string) are not the same
   for (let i = 0; i <= array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -24,22 +23,13 @@ const eqArrays = function (array1, array2) {
   return true;
 };
 
-const assertArraysEqual = function (array1, array2) {
-  // determining if both arrays are not equal in length
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  return true;
-};
-
 ////////////////////////////////////////////////////
-
 console.log("-----------------------------------");
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
 console.log("-----------------------------------");
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
 console.log("-----------------------------------");
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 console.log("-----------------------------------");
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 console.log("-----------------------------------");
