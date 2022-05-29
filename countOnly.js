18c74feda810e4047c7084a171b8cf3047d93b1c
Robert Servado <servadorobertgmail.com>
2022-05-29 18:20:06 -0400

    Running the loop for each item on the firstNames and the assertion comparison

--- countOnly.js
+++ countOnly.js
@@ -15,6 +15,10 @@ const assertEqual = function (actual, expected) {
 const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
+  for (const item of allItems) {
+    console.log(item);
+  }
+
   return results;
 };
 
