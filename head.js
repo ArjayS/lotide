558bf35f0e8f7f26bf2802be4307c2f63d7996c3
Robert Servado <servadorobertgmail.com>
2022-06-09 21:16:37 -0400

    Refactored head.js again by removing the commented out arrayEqual function

--- head.js
+++ head.js
@@ -1,17 +1,3 @@
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//     console.log(
-//       `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
-//     );
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//     console.log(
-//       `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
-//     );
-//   }
-// };
-
 const assertEqual = require("./assertEqual");
 
 const head = function (array) {
