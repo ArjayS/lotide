3f47dd27d5f8b94144d871b5d844fbbf07dc5449
Robert Servado <servadorobertgmail.com>
2022-06-09 21:41:13 -0400

    add eqArraysTest.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+console.log("-----------------------------------");
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+console.log("-----------------------------------");
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+console.log("-----------------------------------");
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+console.log("-----------------------------------");
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+console.log("-----------------------------------");
