320f88bc63317c47b8094f7389ac2688195e92da
Robert Servado <servadorobertgmail.com>
2022-06-09 23:55:50 -0400

    Refactor tailTest.js by using Mocha and Chai and remove the comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,3 @@
-// const assertEqual = require("../assertEqual");
-// const tail = require("../tail");
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-
 const assert = require("chai").assert;
 const tail = require("../tail");
 
