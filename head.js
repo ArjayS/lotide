6009c0f3caa31b353b9c553d53f8f09fbae2331d
Robert Servado <servadorobertgmail.com>
2022-06-09 21:04:10 -0400

    Refactored to add the modularizing of head.js

--- head.js
+++ head.js
@@ -1,27 +1,21 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    console.log(
-      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
-    );
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-    console.log(
-      `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
-    );
-  }
-};
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//     console.log(
+//       `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
+//     );
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+//     console.log(
+//       `The type of operand for actual is a ${typeof actual}, and the type of operand for expected is ${typeof expected}`
+//     );
+//   }
+// };
+
+const assertEqual = require("./assertEqual");
 
 const head = function (array) {
   return array[0];
 };
 
-console.log("---------------------------");
-assertEqual(head([5, 6, 7]), 5);
-console.log("---------------------------");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log("---------------------------");
-assertEqual(head([5]), 5);
-console.log("---------------------------");
-assertEqual(head([]), 5);
-console.log("---------------------------");
+module.exports = head;
